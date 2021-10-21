d1054bbfd7ff1b786d8925f73d51e68c55b9ae2f
Sayed <sayed4931@gmail.com>
2021-10-21 11:52:47 -0700

    Project 2 ended and 3 started

---
+++ Project3/test_script.py
@@ -0,0 +1,33 @@
+import numpy as np
+from sklearn.datasets import make_moons
+import neural_network as nn
+
+# Generate a dataset and plot it
+np.random.seed(0)
+X, y = make_moons(200, noise=0.20)
+plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
+
+#decision boundary plotting funcion
+def plot_decision_boundary(pred_func, X, y):
+    # Set min and max values and give it some padding
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    h = 0.01
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole gid
+    Z = pred_func(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral)
+
+
+plt.figure(figsize=(16, 32))
+hidden_layer_dimensions = [1, 2, 3, 4]
+for i, nn_hdim in enumerate(hidden_layer_dimensions):
+    plt.subplot(5, 2, i+1)
+    plt.title('HiddenLayerSize%d' % nn_hdim)
+    model = nn.build_model(X, y, nn_hdim)
+    plot_decision_boundary(lambda x: predict(model, x), X, y)
+plt.show()
