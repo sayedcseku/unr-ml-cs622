0c39aadc6e68da4b6c7541aab2a5636ae67e0a00
Abu Sayed <sayed4931@gmail.com>
2021-11-12 15:59:13 -0800

    dimetionality miss-match

--- Project3/neural_network.py
+++ Project3/neural_network.py
@@ -39,12 +39,14 @@ def predict(model, X):
 
 def build_model(X,y,nn_hdim, num_passes=20000, print_loss=False):
     
-    #nn_hdim = 500
-    W1 = np.random.randn(2,nn_hdim) 
+    nn_hdim = 500
+    input_dim = 2
+    output_dim = 2
+    W1 = np.random.randn(input_dim,nn_hdim) 
     b1 = np.zeros((1,nn_hdim))
     
-    W2 = np.random.randn(nn_hdim,2)
-    b2 = np.zeros((1,2))
+    W2 = np.random.randn(nn_hdim,output_dim)
+    b2 = np.zeros((1,output_dim))
     
     y_2d = np.eye(2)[y] #converting 1d y to 2d y
     
@@ -58,25 +60,41 @@ def build_model(X,y,nn_hdim, num_passes=20000, print_loss=False):
         h = tanh(a)
     
         y_pred = predict(model, X)
-        loss = calculate_loss(model, X, y_2d)
+        #loss = calculate_loss(model, X, y_2d)
+        
+        
         
         #Backpropagation
-        dy = y_pred - y_2d
+        #dy = y_pred - y_2d
+        dy = y_pred
+        
+        for j in range(len(y_pred)):
+            # if the label = 0 then y_hat - y apply for first output node
+            if y[j] == 0:
+                dy[j, 0] = y_pred[j, 0] - 1
+                dy[j, 1] = y_pred[j, 1]
+            # if the label = 1 then second output, y_hat - y
+            else:
+                dy[j, 0] = y_pred[j, 0]
+                dy[j, 1] = y_pred[j, 1] - 1
         
         dA = (1-tanh(a)*tanh(a)) * np.dot(dy, W2.T)
         
         dW2 = np.dot(h.T,dy)
         
-        db2 = dy
+        db2 = np.sum(dy, axis=0)
         
         dW1 = np.dot(X.T, dA)
         
-        db1 = dA
+        db1 = np.sum(dA,axis=0)
         
         W1 = W1 - 0.01 * dW1
         b1 = b1 - 0.01 * db1
         
         W2 = W2 - 0.01 * dW2
         b2 = b2 - 0.01 * db2
+        
+        if print_loss and i % 1000 == 0:
+            print("Iteration : ", i, "Loss : ", calculate_loss(model, X, y_2d))
         
     return model
